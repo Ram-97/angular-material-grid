a15b6604bf790c3ec02d77d4740607cc73dc770b
Ram-97 <ram031197@gmail.com>
2021-05-16 12:05:12 +0530

    Grid Lib build command added

--- projects/grid/src/public-api.ts
+++ projects/grid/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of grid
  */
 
-export * from './lib/grid.service';
+export * from './lib/grid.model';
 export * from './lib/grid.component';
 export * from './lib/grid.module';
